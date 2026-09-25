b01e1672801cdc6659ed7202deee9e97bceacd09
kprince88498 <kprince88498@gmail.com>
2026-09-25 09:53:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// अभी के लिए हम सीधे यहीं पेजेस का डमी ढांचा बना रहे हैं, बाद में इन्हें अलग फाइल्स में डालेंगे
-const HomePlaceholder = () => <div className="p-8 text-white">VryonAI Home Page (Coming Soon)</div>;
-const DashboardPlaceholder = () => <div className="p-8 text-white">VryonAI Advanced Dashboard (Coming Soon)</div>;
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* मुख्य रास्ते (Routes) */}
-        <Route path="/" element={<HomePlaceholder />} />
-        <Route path="/dashboard" element={<DashboardPlaceholder />} />
-      </Routes>
+      <div className="min-h-screen bg-[#060A17] text-white">
+        <AppRoutes />
+      </div>
     </BrowserRouter>
   );
 }
